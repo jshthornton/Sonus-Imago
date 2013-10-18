d841a7db1c981fc544a09d9886320b454212551f
Josh Thornton <jshthornton@gmail.com>
2013-10-18 16:29:58 +0100

    Including required libraries. Require.js now has content script shim. Prototype require.js config

--- src/content/scripts/main.js
+++ src/content/scripts/main.js
@@ -1,3 +1,27 @@
 ;(function(win, undefined) {
 	'use strict';
+
+	var SHARED_PATH = '../../shared',
+		SHARED_SCRIPTS_PATH = SHARED_PATH + '/scripts';
+
+	require.config({
+		baseUrl: chrome.extension.getURL('/content/scripts'),
+
+		paths: {
+			'jquery': SHARED_SCRIPTS_PATH + '/libs/jquery',
+			'Class': SHARED_SCRIPTS_PATH + '/libs/Class',
+			'_': SHARED_SCRIPTS_PATH + '/libs/lodash',
+			'text': SHARED_SCRIPTS_PATH + '/libs/text'
+		},
+
+		shim: {
+			Class: []
+		}
+	});
+
+	require([
+		'jquery'
+	], function($) {
+		console.log('hello world!')
+	});
 }(window));
