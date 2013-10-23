08c0acfec216489e7be05acf929e4b5c41a5fb11
Joshua Thornton <jshthornton@gmail.com>
2013-10-23 14:48:50 +0100

    Test pages. Detect key press. Find focused element. Change all images to tabbable.

--- src/content/scripts/main.js
+++ src/content/scripts/main.js
@@ -22,6 +22,53 @@
 	require([
 		'jquery'
 	], function($) {
-		console.log('hello world!');
+		function tabbableImage($imgs) {
+			$imgs.prop('tabIndex', 0);
+		};
+
+
+		$(document).ready(function() {
+			tabbableImage($('img'));
+
+			var observer = new WebKitMutationObserver(function(mutations) {
+				var insertedNodes = [];
+
+				mutations.forEach(function(mutation) {
+					for (var i = 0; i < mutation.addedNodes.length; i++) {
+						var addedNode = mutation.addedNodes[i];
+						if(addedNode.nodeName !== 'IMG') continue;
+						
+						insertedNodes.push(addedNode);
+					}
+				});
+
+				tabbableImage($(insertedNodes));
+			});
+
+			observer.observe(document.body, { childList: true });
+
+			$(document).on('keyup', function(e) {
+				var keyCode = e.keyCode,
+					$active,
+					$img;
+
+				if(keyCode !== 120) return;
+
+				$active = $(document.activeElement);
+				console.log($active);
+
+				if(!$active.length) return;
+				
+				//Is img?
+				if($active[0].nodeName !== 'IMG') return;
+
+				$img = $active;
+				$active = null;
+
+				//var $canvas = $('<canvas />');
+				//console.log($canvas);
+			});
+		});
+
 	});
 }(window));
