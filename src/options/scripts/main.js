9c03a58acc4ef7e1222227e24237c8775b6b0656
Joshua Thornton <jshthornton@gmail.com>
2013-11-14 17:13:47 +0000

    Shimminh backbone.

---
+++ src/options/scripts/main.js
@@ -0,0 +1,36 @@
+(function(win, undefined) {
+	'use strict';
+
+	var SHARED_PATH = '../../shared',
+		SHARED_SCRIPTS_PATH = SHARED_PATH + '/scripts';
+
+	require.config({
+		baseUrl: chrome.extension.getURL('/content/scripts'),
+
+		paths: {
+			'jquery': SHARED_SCRIPTS_PATH + '/libs/jquery',
+			'Class': SHARED_SCRIPTS_PATH + '/libs/Class',
+			'_': SHARED_SCRIPTS_PATH + '/libs/lodash',
+			'text': SHARED_SCRIPTS_PATH + '/libs/text',
+			'Backbone': SHARED_SCRIPTS_PATH + '/libs/Backbone'
+		},
+
+		shim: {
+			Class: [],
+			
+			Backbone: {
+				deps: ['_', 'jquery'],
+				exports: 'Backbone'
+			}
+		}
+	});
+
+	require([
+		'jquery'
+	], function($) {
+		
+		$(document).ready(function() {
+			
+		});
+	});
+}(window));
