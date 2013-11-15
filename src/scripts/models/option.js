dc57113543fb67e92bff1a15283821e24cf78fb7
Joshua Thornton <jshthornton@gmail.com>
2013-11-15 02:11:03 +0000

    Using model volume. Volume sync.

--- src/scripts/models/option.js
+++ src/scripts/models/option.js
@@ -2,7 +2,11 @@ define([
 	'Backbone'
 ], function(Backbone) {
 	var M = Backbone.Model.extend({
-
+		defaults: {
+			gridSize: 5,
+			volume: 80,
+			moodPack: 'gb'
+		}
 	});
 
 	return new M();
--- src/scripts/options/views/OptionsView.js
+++ src/scripts/options/views/OptionsView.js
@@ -3,23 +3,49 @@ define([
 	'Backbone',
 	'text!./templates/Options.jst',
 	'models/option'
-], function(_, Backbone, template, Option) {
+], function(_, Backbone, template, option) {
 
 	var V = Backbone.View.extend({
+		//$volumeText
+		//$volumeRange
 
 		initialize: function() {
-
 			this.render();
 		},
 
 		render: function() {
 			var compiled = _.template(template),
-				output = compiled({}),
+				output = compiled({
+					option: option
+				}),
 				$tmpl = $(output);
 
 
-
 			$('main', this.$el).html($tmpl);
+		},
+
+		//Events
+		events: {
+			'change #grid-size': 'onGridSizeChange',
+			'change .volume': 'onVolumeChange'
+		},
+
+		onGridSizeChange: function(e) {
+			var select = e.currentTarget;
+			console.log(this, select);
+		},
+
+		onVolumeChange: function(e) {
+			var val = e.currentTarget.value;
+
+			this.$volumeText = this.$volumeText || $('#volume', this.$el);
+			this.$volumeRange = this.$volumeRange || $('#volume-range', this.$el);
+
+			if(this.$volumeText[0] !== e.currentTarget) {
+				this.$volumeText[0].value = val;
+			} else if(this.$volumeRange[0] !== e.currentTarget) {
+				this.$volumeRange[0].value = val;
+			}
 		}
 	});
 
