c6024e0aed6f69f49c0a3a6595a4f19602373d1d
Joshua Thornton <jshthornton@gmail.com>
2014-04-14 15:39:25 +0100

    Fixed the issue whereby install will not run in an async manner. Renamed project.

--- src/scripts/background/main.js
+++ src/scripts/background/main.js
@@ -1,103 +1,113 @@
-require([
-	'underscore'
-], function(_) {
-	var background = {
-		init: function() {
-			_.bindAll(this);
+(function() {
+	var installFlag = false;
 
-			this.setupPiano();
+	chrome.runtime.onInstalled.addListener(function() {
+		installFlag = true;
+	});
 
-			chrome.runtime.onInstalled.addListener(this.onInstall);
-			chrome.runtime.onMessage.addListener(this.onMessage);
-		},
+	require([
+		'underscore'
+	], function(_) {
+		var background = {
+			init: function() {
+				_.bindAll(this);
 
-		setupPiano: function() {
-			require([
-				'libs/music',
-				'libs/instruments/piano'
-			], function(music, piano) {
-				var audioContext = music.getAudioContext();
-				piano.process(audioContext);
-			});
-		},
+				this.setupPiano();
 
-		onInstall: function() {
-			require([
-				'collections/options'
-			], function(options) {
-				options.localStorage._clear(); //@TODO: remove, dev only code.
-
-				if(options.length === 0) {
-					//Initial Setup
-					options.resetInitial();
-					options.saveAll();
+				if(installFlag) {
+					this.onInstalled;
 				}
-			});
-		},
 
-		onMessage: function(request, sender, sendResponse) {
-			var _this = this;
-			console.groupCollapsed('Message');
+				chrome.runtime.onMessage.addListener(this.onMessage);
+			},
 
-			switch(request.type) {
-				case 'options':
-					require([
-						'collections/options',
-					], function(options) {
-						options.fetch({
-							reset: true,
-							success: function() {
-								var _options = _.indexBy(options.models, 'id');
+			setupPiano: function() {
+				require([
+					'libs/music',
+					'libs/instruments/piano'
+				], function(music, piano) {
+					var audioContext = music.getAudioContext();
+					piano.process(audioContext);
+				});
+			},
 
-								sendResponse(JSON.stringify(_options));
-								console.groupEnd();
-							}
-						});
-					});
+			onInstalled: function() {
+				require([
+					'collections/options'
+				], function(options) {
+					options.localStorage._clear(); //@TODO: remove, dev only code.
 
-					break;
+					if(options.length === 0) {
+						//Initial Setup
+						options.resetInitial();
+						options.saveAll();
+					}
+				});
+			},
 
-				case 'harmonise':
-					require([
-						'background/ImageAnalyser',
-						'collections/options',
-						'collections/moodPacks',
-						'libs/music',
-						'libs/instruments/piano'
-					], function(ImageAnalyser, options, moodPacks, music, piano) {
-						options.fetch({
-							reset: true,
-							success: function() {
-								var moodPackId = options.get('moodPack').get('value'),
-									moodPack = moodPacks.get(moodPackId);
+			onMessage: function(request, sender, sendResponse) {
+				var _this = this;
+				console.groupCollapsed('Message');
 
-								if(music.get('playing') === true) {
-									music.stop(true);
-									return;
+				switch(request.type) {
+					case 'options':
+						require([
+							'collections/options',
+						], function(options) {
+							options.fetch({
+								reset: true,
+								success: function() {
+									var _options = _.indexBy(options.models, 'id');
+
+									sendResponse(JSON.stringify(_options));
+									console.groupEnd();
 								}
+							});
+						});
 
-								var imageAnalyser = new ImageAnalyser(request.imageSrc);
-								imageAnalyser.analyse().then(function(segments) {
+						break;
+
+					case 'harmonise':
+						require([
+							'background/ImageAnalyser',
+							'collections/options',
+							'collections/moodPacks',
+							'libs/music',
+							'libs/instruments/piano'
+						], function(ImageAnalyser, options, moodPacks, music, piano) {
+							options.fetch({
+								reset: true,
+								success: function() {
+									var moodPackId = options.get('moodPack').get('value'),
+										moodPack = moodPacks.get(moodPackId);
+
+									if(music.get('playing') === true) {
+										music.stop(true);
+										return;
+									}
 
 									var pianoState = piano.ready.state();
 
-									if(pianoState !== 'resolved') {
-										if(pianoState === 'pending') {
-											_this.sendFlashMessage({
-												tab: sender.tab, 
-												msg: 'Waiting for music to initialise', 
-												type: 'notice'
-											});
-										} else if(pianoState === 'rejected') {
-											_this.sendFlashMessage({
-												tab: sender.tab, 
-												msg: 'Music failed to initialise, try reloading Sonus Imago extension', 
-												type: 'error'
-											});
-										}
+									if(pianoState === 'pending') {
+										_this.sendFlashMessage({
+											tab: sender.tab, 
+											msg: 'Waiting for music to initialise', 
+											type: 'notice'
+										});
+										return;
+									} 
+
+									if(pianoState === 'rejected') {
+										_this.sendFlashMessage({
+											tab: sender.tab, 
+											msg: 'Music failed to initialise, try reloading Sonus Imago extension', 
+											type: 'error'
+										});
+										return;
 									}
 
-									piano.ready.then(function() {
+									var imageAnalyser = new ImageAnalyser(request.imageSrc);
+									imageAnalyser.analyse().then(function(segments) {
 										var music = moodPack.generateMusic(segments);
 
 										music.onFinished(function() {
@@ -108,25 +118,26 @@ require([
 
 										music.play();
 									});
-								});
-							}
+								}
+							});
 						});
-					});
 
-					break;
+						break;
+				}
+
+				return true;
+			},
+
+			sendFlashMessage: function(opts) {
+				chrome.tabs.sendMessage(opts.tab.id, {
+					cmd: 'flashMessage',
+					msg: opts.msg,
+					type: opts.type
+				});
 			}
+		};
 
-			return true;
-		},
+		background.init();
+	});
+}());
 
-		sendFlashMessage: function(opts) {
-			chrome.tabs.sendMessage(opts.tab.id, {
-				cmd: 'flashMessage',
-				msg: opts.msg,
-				type: opts.type
-			});
-		}
-	};
-
-	background.init();
-});
