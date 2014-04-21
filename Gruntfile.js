dd046b22177af5fa7b0d89ec914f74bde0bc0fc9
Joshua Thornton <jshthornton@gmail.com>
2014-04-21 12:58:46 +0100

    Starting optimisation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,45 @@
 					'!<%= pkg.src %>/scripts/require-cs.js',
 					'<%= pkg.src %>/scripts/libs/instruments/*.js'
 				],
+			},
+
+			clean: {
+				main: ['<%= pkg.dist %>/*'],
+				after: [
+					'<%= pkg.dist %>/scripts/*',
+					'!<%= pkg.dist %>/scripts/app.js'
+				]
+			}
+
+			copy: {
+				main: {
+					files: [
+						{
+							expand: true, 
+							cwd: '<%= pkg.src %>/', 
+							src: ['**'], 
+							dest: '<%= pkg.dist %>/'
+						} // includes files in path and its subdirs
+					]
+				},
+			},
+
+			requirejs: {
+				compile: {
+					options: {
+						baseUrl: './',
+						mainConfigFile: 'build.js',
+						name: 'path/to/almond', // assumes a production build using almond
+						out: 'path/to/optimized.js'
+					}
+				}
 			}
 		});
 
 		grunt.loadNpmTasks('grunt-contrib-jshint');
 		grunt.loadNpmTasks('grunt-contrib-clean');
+		grunt.loadNpmTasks('grunt-contrib-requirejs');
+		grunt.loadNpmTasks('grunt-contrib-copy');
 
 		//Task to run all jshints
 		grunt.registerTask('jshint:all', ['jshint:grunt', 'jshint:src']);
